b8d2d7d5f08d9cfe2bc9046e61c0dc81c532ea7d
Roberto Casadei <robyonrails@gmail.com>
2020-04-18 22:11:17 +0200

    Minor addition

--- src/main/cpp/app.cpp
+++ src/main/cpp/app.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include "app.h"
 
 std::string learning_cpp::Greeter::greeting() {
     return std::string("Hello, World!");
 }
 
+namespace foo {
+    void bar();
+}
+
+void foo::bar(){ std::cout << "foo:bar" << std::endl; }
+
+using namespace foo;
+
 int main () {
     learning_cpp::Greeter greeter;
     std::cout << greeter.greeting() << std::endl;
+    bar();
     return 0;
 }
